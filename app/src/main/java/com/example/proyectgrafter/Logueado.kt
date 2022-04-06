c66d221f3e9f9f67c1b11258b50314eb0b4998da
Maken27 <matakius@gmail.com>
2022-04-05 23:06:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectgrafter/Logueado.kt
@@ -0,0 +1,11 @@
+package com.example.proyectgrafter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Logueado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logueado)
+    }
+}
